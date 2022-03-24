5a437e69161e67957995c71be95584ff09178e32
Agustin <agustinsurila@gmail.com>
2022-03-24 18:59:21 -0300

    Importamos la conexion a la DB, datos en variables de entorno, enrutado dinamico y el uso de un util para encontrar endpoint erroneos

--- app.js
+++ app.js
@@ -3,15 +3,23 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 
+const dbConnect = require("./config/mongo");
+const unknownEndpoint = require("./utils/unknownEndpoint");
+
 const app = express();
 
 //Middlewares
 app.use(cors());
+app.use(express.json());
 
 //Rutas
-app.get("/", (req, res) => {
-  res.send("<h1>Hola Mundo</h1>");
-});
+
+// app.get("/", (req, res) => {
+//   res.send("<h1>Hola Mundo</h1>");
+// });
+
+app.use("/api", require("./routes"));
+app.use(unknownEndpoint);
 
 //Iniciando Servidor
 
@@ -20,3 +28,6 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
+
+//Invocando la conexion a la DB
+dbConnect();
