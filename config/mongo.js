137c053eae7c522a1e957a896bbd149d91f9dffe
Agustin <agustinsurila@gmail.com>
2022-03-29 19:58:18 -0300

    Fixed connect object

--- config/mongo.js
+++ config/mongo.js
@@ -1,29 +1,19 @@
 const mongo = require("mongoose");
 
 //Creando función para conectarnos a la DB
+require("dotenv").config();
 
 const dbConnect = () => {
   const DB_URI = process.env.DB_URI;
 
   //Le decimos a mongo que se conecte a la base de datos.
-  mongo.connect(
-    DB_URI,
-    {
-      useNewUrlParse: true,
+  mongo
+    .connect(DB_URI, {
+      useNewUrlParser: true,
       useUnifiedTopology: true,
-    },
-    (err, res) => {
-      try {
-        console.log("Conectado Correctamente a la DB");
-      } catch (err) {
-        console.log(`No se pudo conectar a la DB:`);
-        res.status(400).send({
-          errMessage: "No se pudo conectar a la DB",
-          err,
-        });
-      }
-    },
-  );
+    })
+    .then(() => console.log("Conectado Correctamente a la DB"))
+    .catch((err) => console.log(`No se pudo conectar a la DB:`, err));
 };
 
 module.exports = dbConnect;
